531de1bdc7f41af3c4d1903827fa3c1f9d458735
DevAgui <dagmartin98@gmail.com>
2022-10-10 13:32:47 +0200

    Círculo añadido

--- Practicas/Program.cs
+++ Practicas/Program.cs
@@ -10,6 +10,25 @@ namespace Practicas
     {
         static void Main(string[] args)
         {
+             
+            Practica1();
+
+            
         }
+        #region Practica1
+        static void Practica1()
+        {
+            Console.WriteLine("Éste programa te dice el área de un círculo");
+
+            Console.WriteLine("Introduce el primer número");
+            double radio = int.Parse(Console.ReadLine());
+
+            const double PI = 3.14;
+
+            double resultado = PI * Math.Pow(radio, 2);
+
+            Console.WriteLine("El área del círculo de radio {0} es de: {1}", radio, resultado);
+        }
+        #endregion
     }
 }
